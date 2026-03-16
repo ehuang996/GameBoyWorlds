e378424897946cbfae75df51c57e0457f12e6107
Dhananjay Ashok <dhananjay.ashok99@gmail.com>
2026-03-16 13:26:37 -0700

    Automatic commit: Added all

--- dev/dev_play.py
+++ dev/dev_play.py
@@ -10,20 +10,20 @@ import click
     help="Variant of the Pokemon game to emulate.",
 )
 @click.option(
-    "--tracker_variant",
+    "--state_tracker_class",
     type=str,
     default="default",
-    help="Variant of the tracker to use.",
+    help="Variant of the state tracker to use.",
 )
 @click.option(
     "--init_state", type=str, default=None, help="Name of the initial state file"
 )
-def main(game, tracker_variant, init_state):
+def main(game, state_tracker_class, init_state):
     env = get_emulator(
         game=game,
         init_state=init_state,
         headless=False,
-        state_tracker_class=tracker_variant,
+        state_tracker_class=state_tracker_class,
     )
     env._dev_play()
 
